913b8d17b2d50710492dc959a1765479869f3c4c
Eric Hyche <erich@noom.com>
2022-05-10 09:07:30 -0400

    Use buildCommand instead of prebuildCommand

--- Example 1 - SwiftGen/Plugins/SwiftGenPlugin/plugin.swift
+++ Example 1 - SwiftGen/Plugins/SwiftGenPlugin/plugin.swift
@@ -13,11 +13,46 @@ struct SwiftGenPlugin: BuildToolPlugin {
         let swiftGenOutputsDir = context.pluginWorkDirectory.appending("SwiftGenOutputs")
         try FileManager.default.createDirectory(atPath: swiftGenOutputsDir.string, withIntermediateDirectories: true)
 
+        // Specify the input files
+        let swiftGenStringsInputFile = context.package.directory.appending(["Sources", target.name, "Resources", "en.lproj"])
+        let swiftGenAssetsInputFile = context.package.directory.appending(["Sources", target.name, "Resources", "Media.xcassets"])
+
+        // Specify the output files
+        let swiftGenStringsOutputFile = swiftGenOutputsDir.appending("Strings.swift")
+        let swiftGenAssetsOutputFile = swiftGenOutputsDir.appending("Media.swift")
+
+        var inputFiles = [Path]()
+        var outputFiles = [Path]()
+        if FileManager.default.fileExists(atPath: swiftGenStringsInputFile.string) {
+            inputFiles.append(swiftGenStringsInputFile)
+            outputFiles.append(swiftGenStringsOutputFile)
+        }
+        if FileManager.default.fileExists(atPath: swiftGenAssetsInputFile.string) {
+            inputFiles.append(swiftGenAssetsInputFile)
+            outputFiles.append(swiftGenAssetsOutputFile)
+        }
+
         // Return a command to run `swiftgen` as a prebuild command. It will be run before
         // every build and generates source files into an output directory provided by the
         // build context.
         return [
-            .prebuildCommand(
+//            .prebuildCommand(
+//                displayName: "Running SwiftGen",
+//                executable: try context.tool(named: "swiftgen").path,
+//                arguments: [
+//                    "config",
+//                    "run",
+//                    "--verbose",
+//                    "--config", "\(swiftGenConfigFile)"
+//                ],
+//                environment: [
+//                    "PROJECT_DIR": "\(context.package.directory)",
+//                    "TARGET_NAME": "\(target.name)",
+//                    "DERIVED_SOURCES_DIR": "\(swiftGenOutputsDir)",
+//                ],
+//                outputFilesDirectory: swiftGenOutputsDir
+//            ),
+            .buildCommand(
                 displayName: "Running SwiftGen",
                 executable: try context.tool(named: "swiftgen").path,
                 arguments: [
@@ -31,8 +66,9 @@ struct SwiftGenPlugin: BuildToolPlugin {
                     "TARGET_NAME": "\(target.name)",
                     "DERIVED_SOURCES_DIR": "\(swiftGenOutputsDir)",
                 ],
-                outputFilesDirectory: swiftGenOutputsDir
-            )
+                inputFiles: inputFiles,
+                outputFiles: outputFiles
+            ),
         ]
     }
 }
